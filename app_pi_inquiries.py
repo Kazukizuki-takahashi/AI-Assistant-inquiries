27ebfa3c5c58276a3496f9c0e5008a1d0ff34a8c
Kazukizuki-takahashi <140982011+Kazukizuki-takahashi@users.noreply.github.com>
2023-09-06 09:29:09 +0900

    Add files via upload

--- app_pi_inquiries.py
+++ app_pi_inquiries.py
@@ -5,5 +5,4 @@ from streamlit.components.v1 import html
 # サービス名を表示する
 st.title("お問い合わせ")
 
-html('<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScHlR9LYv3fmFuhHP0uqwX3SOLJYvELtfz-a0G_VAh5JJPnrw/viewform?embedded=true" width="640" height="5000"></iframe>')
-
+html('<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScHlR9LYv3fmFuhHP0uqwX3SOLJYvELtfz-a0G_VAh5JJPnrw/viewform?embedded=true" width="100%" height="2000"></iframe>')
